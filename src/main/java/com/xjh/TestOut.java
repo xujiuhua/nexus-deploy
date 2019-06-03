1d00967db0a0072912acbaeb3d19a9252b5604a1
jiuhua.xu <xujiuhuamoney@163.com>
2019-06-03 10:14:16 +0800

    Initial commit

---
+++ src/main/java/com/xjh/TestOut.java
@@ -0,0 +1,16 @@
+package com.xjh;
+
+/**
+ * <p></p>
+ *
+ * @author jiuhua.xu
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class TestOut {
+
+    public void out(String s) {
+        System.out.println(s);
+    }
+
+}
