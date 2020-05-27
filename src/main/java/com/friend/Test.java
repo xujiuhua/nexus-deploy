a56d82ea7ece9ff364bd326aa583e8f7521c5cca
jiuhua.xu <xujiuhuamoney@163.com>
2020-05-27 12:00:42 +0800

    [Feature] 测试下

---
+++ src/main/java/com/friend/Test.java
@@ -0,0 +1,14 @@
+package com.friend;
+
+/**
+ * <p></p>
+ *
+ * @author jiuhua.xu
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class Test {
+    private void print() {
+        System.out.println("Nexus Print");
+    }
+}
